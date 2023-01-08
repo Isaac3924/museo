7dfb0f3e5d4276a185e796e5889c62395b904288
Isaac Alter <i_alter@yahoo.com>
2023-01-07 23:09:11 -0700

    Add feat: attribute set in photograph_spec

--- spec/photograph_spec.rb
+++ spec/photograph_spec.rb
@@ -2,5 +2,12 @@ require 'rspec'
 require './lib/photograph'
 
 RSpec.describe Photograph do
+    let(:attributes) = {
+        id: "1",
+        name: "Rue Mouffetard, Paris (Boy with Bottles)",
+        artist_id: "4",
+        year: "1954"
+    }
+
     
 end
